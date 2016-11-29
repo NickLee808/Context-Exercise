cc1f0195e8e3c415784fd48077a80a47edccd5eb
Nick Lee <nicklee808@gmail.com>
2016-11-28 16:58:11 -1000

    #3

--- context.js
+++ context.js
@@ -15,7 +15,6 @@ var megaman = new Robot("Mega Man");
 var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
-
 // #1
 console.log(add(0,1));
 
@@ -23,8 +22,8 @@ console.log(add(0,1));
 console.log(add.call(voltron,2,3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus,[20,30]));
 
 // #4
 
-console.log(  );
+console.log();
